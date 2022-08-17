896ab96326db8b3ec86e084f269e83b13b25e22c
Olivia Kim <oliviakim321@gmail.com>
2022-08-17 14:32:58 -0400

    remove unused files, rename podspec (#4)
    * remove unused files, rename podspec
    
    * missed one

--- Example/Pods/Target Support Files/Pods-mobile-sdk_Example/Pods-mobile-sdk_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mobile_sdk_Example : NSObject
-@end
-@implementation PodsDummy_Pods_mobile_sdk_Example
-@end
--- Example/Pods/Target Support Files/mobile-sdk/mobile-sdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_mobile_sdk : NSObject
-@end
-@implementation PodsDummy_mobile_sdk
-@end
