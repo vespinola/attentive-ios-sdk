2901f61c741c0d8acb294753effc83bf846efebf
Wyatt Davis <wyattjoe40@live.com>
2022-12-21 13:48:07 -0800

    Update Example app for new pod version (#35)

--- Example/Example/ImportAttentiveSDK.h
+++ Example/Example/ImportAttentiveSDK.h
@@ -16,6 +16,12 @@
 // Since we build the pod into a static library, we should import all the needed public headers here (i.e. there is no umbrella header file)
 #import "ATTNSDK.h"
 #import "ATTNUserIdentity.h"
+#import "ATTNEventTracker.h"
+#import "ATTNPurchaseEvent.h"
+#import "ATTNItem.h"
+#import "ATTNPrice.h"
+#import "ATTNOrder.h"
+#import "ATTNCart.h"
 // If for some reason we switched to building the pod into a dynamic framework, we can use this umbrella import instead:
 // #import "attentive_ios_sdk/attentive-ios-sdk-umbrella.h"
 #endif
