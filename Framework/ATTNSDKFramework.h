eb6b7178de609edd603d4808ebeb52bac6877d0b
Reagan Smith <rsmith@attentivemobile.com>
2024-02-13 15:52:35 -0500

    Renaming imports for framework to be exact paths

--- Framework/ATTNSDKFramework.h
+++ Framework/ATTNSDKFramework.h
@@ -6,16 +6,17 @@ FOUNDATION_EXPORT const unsigned char ATTNSDKFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <attentive_ios_sdk/PublicHeader.h>
 
-#import <attentive_ios_sdk_framework/ATTNSDK.h>
-#import <attentive_ios_sdk_framework/ATTNUserIdentity.h>
-#import <attentive_ios_sdk_framework/ATTNEventTracker.h>
-#import <attentive_ios_sdk_framework/ATTNPurchaseEvent.h>
-#import <attentive_ios_sdk_framework/ATTNItem.h>
-#import <attentive_ios_sdk_framework/ATTNPrice.h>
-#import <attentive_ios_sdk_framework/ATTNEvent.h>
-#import <attentive_ios_sdk_framework/ATTNPurchaseEvent.h>
-#import <attentive_ios_sdk_framework/ATTNProductViewEvent.h>
-#import <attentive_ios_sdk_framework/ATTNAddToCartEvent.h>
-#import <attentive_ios_sdk_framework/ATTNOrder.h>
-#import <attentive_ios_sdk_framework/ATTNCart.h>
-#import <attentive_ios_sdk_framework/ATTNCustomEvent.h>
+#import <Sources/ATTNSDK.h>
+//#import <attentive_ios_sdk_framework/ATTNSDK.h>
+//#import <attentive_ios_sdk_framework/ATTNUserIdentity.h>
+//#import <attentive_ios_sdk_framework/ATTNEventTracker.h>
+//#import <attentive_ios_sdk_framework/ATTNPurchaseEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNItem.h>
+//#import <attentive_ios_sdk_framework/ATTNPrice.h>
+//#import <attentive_ios_sdk_framework/ATTNEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNPurchaseEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNProductViewEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNAddToCartEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNOrder.h>
+//#import <attentive_ios_sdk_framework/ATTNCart.h>
+//#import <attentive_ios_sdk_framework/ATTNCustomEvent.h>
--- Sources/include/ATTNSDKFramework.h
+++ Sources/include/ATTNSDKFramework.h
@@ -4,15 +4,16 @@ FOUNDATION_EXPORT double ATTNSDKFrameworkVersionNumber;
 
 FOUNDATION_EXPORT const unsigned char ATTNSDKFrameworkVersionString[];
 
-#import <attentive_ios_sdk_framework/ATTNSDK.h>
-#import <attentive_ios_sdk_framework/ATTNUserIdentity.h>
-#import <attentive_ios_sdk_framework/ATTNEventTracker.h>
-#import <attentive_ios_sdk_framework/ATTNItem.h>
-#import <attentive_ios_sdk_framework/ATTNPrice.h>
-#import <attentive_ios_sdk_framework/ATTNEvent.h>
-#import <attentive_ios_sdk_framework/ATTNPurchaseEvent.h>
-#import <attentive_ios_sdk_framework/ATTNProductViewEvent.h>
-#import <attentive_ios_sdk_framework/ATTNAddToCartEvent.h>
-#import <attentive_ios_sdk_framework/ATTNOrder.h>
-#import <attentive_ios_sdk_framework/ATTNCart.h>
-#import <attentive_ios_sdk_framework/ATTNCustomEvent.h>
+#import <../ATTNSDK.h>
+//#import <attentive_ios_sdk_framework/ATTNSDK.h>
+//#import <attentive_ios_sdk_framework/ATTNUserIdentity.h>
+//#import <attentive_ios_sdk_framework/ATTNEventTracker.h>
+//#import <attentive_ios_sdk_framework/ATTNItem.h>
+//#import <attentive_ios_sdk_framework/ATTNPrice.h>
+//#import <attentive_ios_sdk_framework/ATTNEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNPurchaseEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNProductViewEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNAddToCartEvent.h>
+//#import <attentive_ios_sdk_framework/ATTNOrder.h>
+//#import <attentive_ios_sdk_framework/ATTNCart.h>
+//#import <attentive_ios_sdk_framework/ATTNCustomEvent.h>
