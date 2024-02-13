0bf29e1001e180c0b84fe8e2bd350de7232a8346
Reagan Smith <rsmith@attentivemobile.com>
2024-02-13 16:05:17 -0500

    removing duplicate files and updating imports in the framework within include

--- Sources/include/ATTNSDKFramework.h
+++ Sources/include/ATTNSDKFramework.h
@@ -4,16 +4,15 @@ FOUNDATION_EXPORT double ATTNSDKFrameworkVersionNumber;
 
 FOUNDATION_EXPORT const unsigned char ATTNSDKFrameworkVersionString[];
 
-//#import <../ATTNSDK.h>
-//#import <attentive_ios_sdk_framework/ATTNSDK.h>
-//#import <attentive_ios_sdk_framework/ATTNUserIdentity.h>
-//#import <attentive_ios_sdk_framework/ATTNEventTracker.h>
-//#import <attentive_ios_sdk_framework/ATTNItem.h>
-//#import <attentive_ios_sdk_framework/ATTNPrice.h>
-//#import <attentive_ios_sdk_framework/ATTNEvent.h>
-//#import <attentive_ios_sdk_framework/ATTNPurchaseEvent.h>
-//#import <attentive_ios_sdk_framework/ATTNProductViewEvent.h>
-//#import <attentive_ios_sdk_framework/ATTNAddToCartEvent.h>
-//#import <attentive_ios_sdk_framework/ATTNOrder.h>
-//#import <attentive_ios_sdk_framework/ATTNCart.h>
-//#import <attentive_ios_sdk_framework/ATTNCustomEvent.h>
+#import <../ATTNSDK.h>
+#import <../ATTNUserIdentity.h>
+#import <../ATTNEventTracker.h>
+#import <../ATTNItem.h>
+#import <../ATTNPrice.h>
+#import <../ATTNEvent.h>
+#import <../ATTNPurchaseEvent.h>
+#import <../ATTNProductViewEvent.h>
+#import <../ATTNAddToCartEvent.h>
+#import <../ATTNOrder.h>
+#import <../ATTNCart.h>
+#import <../ATTNCustomEvent.h>
